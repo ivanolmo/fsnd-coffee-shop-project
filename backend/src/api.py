78197a2e9dd3cbceb4fc43292862eec1966aa616
Ivan Olmo <ivan.olmo@gmail.com>
2020-01-25 17:13:43 -0600

    Update error handlers

--- backend/src/api.py
+++ backend/src/api.py
@@ -147,13 +147,50 @@ Example error handling for unprocessable entity
 '''
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": error.description
+    }), 404
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+        "success": False,
+        "error": 422,
+        "message": error.description
+    }), 422
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": error.description
+    }), 400
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": error.description
+    }), 405
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": error.description
+    }), 500
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
